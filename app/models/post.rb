803936ea716854acd9c32e1f5248f6c4480e6240
Alvinluipo <lualvin1234@gmail.com>
2016-05-26 22:30:11 -0400

    adding omniauth

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
+  has_many :users, through: :comments
+  has_many :comments, dependent: :destroy
   validates :title, presence: true, length: {minimum: 5 }
   validates :body, presence: true
 
